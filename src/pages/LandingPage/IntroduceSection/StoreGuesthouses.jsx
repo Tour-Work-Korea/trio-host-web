450fedb2fd0e24118973d7435e0a067e7a1a8985
kwak <sal091625@gmail.com>
2025-12-29 15:40:46 +0900

    feat(#7): 배너 수정

--- src/pages/LandingPage/IntroduceSection/StoreGuesthouses.jsx
+++ src/pages/LandingPage/IntroduceSection/StoreGuesthouses.jsx
@@ -6,18 +6,26 @@ import gh1 from "@assets/images/landing/gh_1.png";
 import gh2 from "@assets/images/landing/gh_2.png";
 import gh3 from "@assets/images/landing/gh_3.png";
 import gh4 from "@assets/images/landing/gh_4.png";
+import gh5 from "@assets/images/landing/gh_5.jpg";
+import gh6 from "@assets/images/landing/gh_6.jpg";
+import gh7 from "@assets/images/landing/gh_7.jpg";
+import gh8 from "@assets/images/landing/gh_8.jpg";
+import gh9 from "@assets/images/landing/gh_9.jpg";
+import gh10 from "@assets/images/landing/gh_10.jpg";
+import gh11 from "@assets/images/landing/gh_11.jpg";
 const gh_cards = [
   { title: "베드라디오 동문점", url: gh1 },
   { title: "백패커스홈", url: gh2 },
   { title: "비지터 게스트하우스", url: gh3 },
   { title: "캡틴 제주 게스트하우스", url: gh4 },
-  { title: "베드라디오 동문점", url: gh1 },
-  { title: "백패커스홈", url: gh2 },
-  { title: "비지터 게스트하우스", url: gh3 },
-  { title: "캡틴 제주 게스트하우스", url: gh4 },
-  { title: "베드라디오 동문점", url: gh1 },
-  { title: "백패커스홈", url: gh2 },
-  { title: "비지터 게스트하우스", url: gh3 },
+  { title: "미조", url: gh5 },
+  { title: "김녕기억", url: gh6 },
+  { title: "알멩이", url: gh7 },
+  { title: "이호웨이브", url: gh8 },
+  { title: "예스준", url: gh9 },
+  { title: "이상한 밤", url: gh10 },
+  { title: "제주 신촌 바다뷰", url: gh11 },
+
   { title: "캡틴 제주 게스트하우스", url: gh4 },
   { title: "베드라디오 동문점", url: gh1 },
   { title: "백패커스홈", url: gh2 },
@@ -46,7 +54,10 @@ export default function StoreGuesthouses() {
       <Slider {...settings}>
         {gh_cards?.slice(0, 7).map((el, id) => (
           <div key={id} className="flex-col flex items-center px-2">
-            <img src={el.url} className="w-full rounded-lg" />
+            <img
+              src={el.url}
+              className="w-full rounded-lg h-[80px] object-cover object-center"
+            />
             <p className="font-medium text-center mt-4 text-base">{el.title}</p>
           </div>
         ))}
